bd9dcc65ebf9ee1df6073caa2431d69b81765b24
Bartosz Ratajczyk <Bartosz.Ratajczyk@itechart-group.com>
2022-10-03 16:41:56 +0200

    R5 - working on displaying punctuation

--- src/pages/game/article/TextContainer.tsx
+++ src/pages/game/article/TextContainer.tsx
@@ -8,13 +8,19 @@ type PropsType = {
 
 const findWords = (text: string) => {
   const wordArray = text.split(" ");
+  const regex = /[\,\.\s]/;
 
   console.log(wordArray);
 
   const bluredWords = wordArray.map((word) => {
-    if (word.length < 5) return word + " ";
+    if (word.length < 6) return word + " ";
 
-    const wordHide = word.split("").map(() => <>&#9646;</>);
+    
+
+    const wordHide = word.split("").map((letter) => {
+      if (!regex.test(letter)) return <>&#9647;</>;
+      return letter;
+    });
 
     return (
       <>
@@ -33,12 +39,14 @@ const findWords = (text: string) => {
 const TextContainer = (props: PropsType) => {
   const { title, text } = props.article;
 
+  const bluredTitle = title.split("").map(() => <>&#9647;</>);
+
   const bluredWords = findWords(text);
 
   return (
     <>
       <Typography variant="h2" sx={{ mb: 2 }}>
-        <BlurredText guessed={false}>{title}</BlurredText>
+        <BlurredText guessed={false}>{bluredTitle}</BlurredText>
       </Typography>
       <Typography variant="body1" sx={{ textAlign: "justify" }}>
         {bluredWords}
--- src/pages/game/article/styled.tsx
+++ src/pages/game/article/styled.tsx
@@ -5,8 +5,9 @@ type BlurredText = {
 };
 
 export const BlurredText = styled("span")<BlurredText>`
+  padding: 0;
+  margin: 0;
   color: ${({ theme }) => theme.palette.primary.main};
   background-color: ${({ theme, guessed }) =>
     guessed ? theme.palette.secondary.main : theme.palette.primary.main};
-  letter-spacing: 1px;
 `;
